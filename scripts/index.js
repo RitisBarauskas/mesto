df60d57d160476639a40eb7b2a5419e27429f1ba
Ritis <jimmyelf@gmail.com>
2021-03-06 20:40:47 +0500

    Карточки создаются автоматически из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,42 @@
+const initialCards = [
+    {
+      name: 'Алапаевск',
+      link: 'images/places/alapaevsk.jpg'
+    },
+    {
+      name: 'Башкирия',
+      link: 'images/places/bashkiria.jpg'
+    },
+    {
+      name: 'Пермский край',
+      link: 'images/places/permsky.jpg'
+    },
+    {
+      name: 'Пещера Чудесница (р. Чусовая)',
+      link: 'images/places/chusovaya.jpg'
+    },
+    {
+      name: 'р. Усьва (Пермский край)',
+      link: 'images/places/usva.jpg'
+    },
+    {
+      name: 'р. Зилим (Башкирия)',
+      link: 'images/places/zilim.jpg'
+    }
+  ];
+
+const placeCardTemplate = document.querySelector('#place-card').content;
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach(function (item) {
+    const placeCardElement = placeCardTemplate.querySelector('.element').cloneNode(true);
+    placeCardElement.querySelector('.element__image').src = item.link;
+    placeCardElement.querySelector('.element__image').alt = item.name;
+    placeCardElement.querySelector('.element__title').textContent = item.name;
+    placesList.append(placeCardElement);
+})
+
+
 let editForm = document.querySelector('.popup');
 let closeButton = editForm.querySelector('.popup__close');
 let saveButton = editForm.querySelector('.popup__button');

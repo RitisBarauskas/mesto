8d1870afcc19384b5a705c78c84ffd050be85916
Ritis <jimmyelf@gmail.com>
2021-03-17 23:49:32 +0500

    Репозиторий подготовлен для финального проекта

---
+++ scripts/validate.js
@@ -0,0 +1,28 @@
+function showInputError(formElement, inputElement, errorMessage) {
+    // Добавляем ошибки
+}
+
+function hideInputError(formElement, inputElement) {
+    // Удаляем ошибки
+}
+
+function checkInputValidity(formElement, inputElement) {
+    //Проверяем на валидацию
+}
+
+function setEventListener(formElement) {
+    // Обрабатываем все поля полченных форм
+}
+
+function enableValidation(arr) {
+    // Обработаем все формы
+}
+
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__button',
+    inactiveButtonClass: 'popup__button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  }); 
